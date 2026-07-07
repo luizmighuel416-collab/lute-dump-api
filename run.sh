a795a19996f6e775401637f370cca2e4bfbab32e
luizmighuel416-collab <luizmighuel416@gmail.com>
2026-07-07 15:38:03 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-# Usage: ./run.sh <input.lua> [out.lua] [extra settings...]
-# Example: ./run.sh sample.lua out.lua debug
-
-INPUT="${1:?Usage: ./run.sh <input.lua> [out.lua] [extra settings...]}"
+INPUT="${1:?Usage: ./run.sh <input> [out]}"
 OUT="${2:-out.lua}"
-shift 2
 
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 export HOOKOP_BIN="$SCRIPT_DIR/lute"
